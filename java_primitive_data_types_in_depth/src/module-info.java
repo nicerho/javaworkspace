d7d3f4602c0c8bd80c77ea61d213eb93a9260dcb
nicerho <131422003+nicerho@users.noreply.github.com>
2023-06-04 10:58:16 +0900

    2

---
+++ java_primitive_data_types_in_depth/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ho
+ *
+ */
+module java_primitive_data_types_in_depth {
+}
---
+++ java_primitive_data_types_in_depth/src/primitive/data/types/BiNumberRunner.java
@@ -0,0 +1,15 @@
+package primitive.data.types;
+
+public class BiNumberRunner {
+
+	public static void main(String[] args) {
+		Binumber numbers = new Binumber(2,3);
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+		numbers.doubleValue();
+		System.out.println(numbers.getNumber1());
+		System.out.println(numbers.getNumber2());
+
+	}
+
+}
